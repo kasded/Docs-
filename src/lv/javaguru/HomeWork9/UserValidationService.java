ae74c574811267571f0e66e9becf9a2efc59b1cf
Kaspars Andersons <kasded@gmail.com>
2019-12-02 20:42:53 +0200

    HW7,HW8,HW9

---
+++ src/lv/javaguru/HomeWork9/UserValidationService.java
@@ -0,0 +1,38 @@
+package lv.javaguru.HomeWork9;
+
+
+public class UserValidationService {
+
+
+    public String name;
+    public String lastName;
+    public int age;
+
+    public UserValidationService(String name, String lastName, int age) {
+        if (isValidName(name)) {
+            throw new IllegalArgumentException("To many symbols or to less in name");
+        } else if (isValidLastName(lastName)) {
+            throw new IllegalArgumentException("To many symbols or to less in Last name");
+        } else if (isValidAge(age)) {
+            throw new IllegalArgumentException("To many symbols or to less in age");
+        }
+
+    }
+
+    private boolean isValidName(String name) {
+        System.out.println(name);
+        return name.length() <= 2 || name.length() >= 14;
+    }
+
+    private boolean isValidLastName(String lastName) {
+        System.out.println(lastName);
+        return lastName.length() <= 2 || lastName.length() >= 14;
+    }
+
+    private boolean isValidAge(int age) {
+        System.out.println(age);
+        return age <= 0 || age >= 119;
+    }
+
+
+}
---
+++ src/lv/javaguru/HomeWork9/UserValidationServiceTest.java
@@ -0,0 +1,12 @@
+package lv.javaguru.HomeWork9;
+
+public class UserValidationServiceTest {
+    public static void main(String[] args) {
+
+        UserValidationService userValidationService = new UserValidationService("Janis", "Ozols", 13);
+        System.out.println(userValidationService);
+
+    }
+
+
+}
